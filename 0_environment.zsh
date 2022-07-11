1f4d734b8ca785c80940d0e19a0de7369a417e80
Janzen Zarzoso <janzenz@users.noreply.github.com>
2022-07-11 21:09:12 +1200

    Add files via upload

---
+++ 0_environment.zsh
@@ -0,0 +1,5 @@
+# Turns off Homebrew analytics
+export HOMEBREW_NO_ANALYTICS=1
+
+# NVM directory
+export NVM_DIR="$HOME/.nvm"
---
+++ 1_profile.zsh
@@ -0,0 +1,6 @@
+# Python Version manager config 
+eval "$(pyenv init --path)"
+
+# Node Version Manager (nvm) config
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
